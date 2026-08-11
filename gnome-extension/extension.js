a4afb3f33b3cd18d9f2222e9a304d60fd8e755cc
Sidhant Aind <sidhant-aind@users.noreply.github.com>
2026-08-11 14:51:02 +0530

    Use GNOME 46 SystemIndicator pattern for 100% stable QuickSettings layout

--- gnome-extension/extension.js
+++ gnome-extension/extension.js
@@ -43,59 +43,56 @@ function getInitialState() {
     return { brightness: 80, contrast: 50, volume: 100, display_mode: 1, mode_name: 'Standard' };
 }
 
-// Full-width Brightness Slider in QuickSettings
-const AcerBrightnessSlider = GObject.registerClass(
-class AcerBrightnessSlider extends QuickSettings.QuickSlider {
-    _init(initialVal) {
+const AcerMonitorIndicator = GObject.registerClass(
+class AcerMonitorIndicator extends QuickSettings.SystemIndicator {
+    _init() {
         super._init();
-        this.iconName = 'display-brightness-symbolic';
-        this.slider.value = initialVal / 100.0;
 
-        let timeout = 0;
-        this.slider.connect('notify::value', () => {
-            let val = Math.round(this.slider.value * 100);
-            if (timeout) GLib.source_remove(timeout);
-            timeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
+        let state = getInitialState();
+
+        // Top right system tray icon
+        this._icon = this._addIndicator();
+        this._icon.icon_name = 'display-brightness-symbolic';
+
+        // Full-width Brightness Slider
+        this._brightSlider = new QuickSettings.QuickSlider();
+        this._brightSlider.iconName = 'display-brightness-symbolic';
+        this._brightSlider.slider.value = state.brightness / 100.0;
+
+        let brightTimeout = 0;
+        this._brightSlider.slider.connect('notify::value', () => {
+            let val = Math.round(this._brightSlider.slider.value * 100);
+            if (brightTimeout) GLib.source_remove(brightTimeout);
+            brightTimeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
                 execCli(`brightness ${val}`);
-                timeout = 0;
+                brightTimeout = 0;
                 return GLib.SOURCE_REMOVE;
             });
         });
-    }
-});
 
-// Full-width Contrast Slider in QuickSettings
-const AcerContrastSlider = GObject.registerClass(
-class AcerContrastSlider extends QuickSettings.QuickSlider {
-    _init(initialVal) {
-        super._init();
-        this.iconName = 'display-symbolic';
-        this.slider.value = initialVal / 100.0;
+        // Full-width Contrast Slider
+        this._contrastSlider = new QuickSettings.QuickSlider();
+        this._contrastSlider.iconName = 'display-symbolic';
+        this._contrastSlider.slider.value = state.contrast / 100.0;
 
-        let timeout = 0;
-        this.slider.connect('notify::value', () => {
-            let val = Math.round(this.slider.value * 100);
-            if (timeout) GLib.source_remove(timeout);
-            timeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
+        let contrastTimeout = 0;
+        this._contrastSlider.slider.connect('notify::value', () => {
+            let val = Math.round(this._contrastSlider.slider.value * 100);
+            if (contrastTimeout) GLib.source_remove(contrastTimeout);
+            contrastTimeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
                 execCli(`contrast ${val}`);
-                timeout = 0;
+                contrastTimeout = 0;
                 return GLib.SOURCE_REMOVE;
             });
         });
-    }
-});
 
-// Presets Menu Toggle Pill
-const AcerPresetsToggle = GObject.registerClass(
-class AcerPresetsToggle extends QuickSettings.QuickMenuToggle {
-    _init(initialModeName) {
-        super._init({
-            title: `Preset: ${initialModeName}`,
+        // Presets Toggle Pill
+        this._presetsToggle = new QuickSettings.QuickMenuToggle({
+            title: `Preset: ${state.mode_name}`,
             iconName: 'video-display-symbolic',
             toggleMode: false,
         });
-
-        this.menu.setHeader('video-display-symbolic', 'Presets');
+        this._presetsToggle.menu.setHeader('video-display-symbolic', 'Presets');
 
         let modes = [
             { label: 'User Mode', shortName: 'User', cmd: 'preset user' },
@@ -109,31 +106,27 @@ class AcerPresetsToggle extends QuickSettings.QuickMenuToggle {
         ];
 
         for (let m of modes) {
-            this.menu.addAction(m.label, () => {
+            this._presetsToggle.menu.addAction(m.label, () => {
                 execCli(m.cmd);
-                this.title = `Preset: ${m.shortName}`;
+                this._presetsToggle.title = `Preset: ${m.shortName}`;
             });
         }
+
+        // Attach quickSettingsItems array to SystemIndicator
+        this.quickSettingsItems = [this._brightSlider, this._contrastSlider, this._presetsToggle];
     }
 });
 
 export default class AcerMonitorExtension extends Extension {
     enable() {
-        let state = getInitialState();
-
-        this._brightSlider = new AcerBrightnessSlider(state.brightness);
-        this._contrastSlider = new AcerContrastSlider(state.contrast);
-        this._presetsToggle = new AcerPresetsToggle(state.mode_name);
-
-        // colSpan = 2 forces 100% full width across the QuickSettings menu!
-        Main.panel.statusArea.quickSettings.addItem(this._brightSlider, 2);
-        Main.panel.statusArea.quickSettings.addItem(this._contrastSlider, 2);
-        Main.panel.statusArea.quickSettings.addItem(this._presetsToggle, 1);
+        this._indicator = new AcerMonitorIndicator();
+        Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
     }
 
     disable() {
-        if (this._brightSlider) { this._brightSlider.destroy(); this._brightSlider = null; }
-        if (this._contrastSlider) { this._contrastSlider.destroy(); this._contrastSlider = null; }
-        if (this._presetsToggle) { this._presetsToggle.destroy(); this._presetsToggle = null; }
+        if (this._indicator) {
+            this._indicator.destroy();
+            this._indicator = null;
+        }
     }
 }
